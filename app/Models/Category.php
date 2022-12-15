de06a7f2ca0aa6554e7e7481279d5843caad83d9
Bruno Costa <bruno.costa@dafiti.com.br>
2022-12-15 20:08:27 -0300

    finalizado

---
+++ app/Models/Category.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory, SoftDeletes, HasUuids;
+
+    public $fillable = [
+        'id',
+        'name',
+        'description',
+        'is_active'
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean'
+    ];
+}
