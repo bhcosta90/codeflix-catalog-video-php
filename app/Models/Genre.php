428a4809e74c6e2bdb2b854b13652e5926579bf1
Bruno Costa <bruno.costa@dafiti.com.br>
2022-12-20 15:01:38 -0300

    feat: 8020

---
+++ app/Models/Genre.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Genre extends Model
+{
+    use HasFactory, SoftDeletes, HasUuids;
+
+    public $fillable = [
+        'id',
+        'name',
+        'is_active'
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean'
+    ];
+}
