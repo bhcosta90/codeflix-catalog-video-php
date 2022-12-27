1cc545bb8e16ad8313d4b77bb982bc1a7431ebc7
Bruno Costa <bruno.costa@dafiti.com.br>
2022-12-27 12:33:53 -0300

    implementando

--- app/Services/FileStorage.php
+++ app/Services/FileStorage.php
@@ -3,16 +3,29 @@
 namespace App\Services;
 
 use Costa\DomainPackage\UseCase\Interfaces\FileStorageInterface;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class FileStorage implements FileStorageInterface
 {
     public function store(string $path, array $file): string
     {
-        //
+        $contents = $this->convertFileToLaravelFile($file);
+        return Storage::put($path, $contents);
     }
 
     public function remove(string $path): bool
     {
-        //
+        return (bool) Storage::delete($path);
+    }
+
+    protected function convertFileToLaravelFile(array $file): UploadedFile
+    {
+        return new UploadedFile(
+            path: $file['tmp_name'],
+            originalName: $file['name'],
+            mimeType: $file['type'],
+            error: $file['error'],
+        );
     }
 }
